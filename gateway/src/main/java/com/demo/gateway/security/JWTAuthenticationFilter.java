bf11bdef9584c33aa04ede21a2d34e2e6662f35c
KhoaDoanZXC <brian42storm@gmail.com>
2021-09-26 15:00:28 +0700

    Connect gateway to user

--- gateway/src/main/java/com/demo/gateway/security/JWTAuthenticationFilter.java
+++ gateway/src/main/java/com/demo/gateway/security/JWTAuthenticationFilter.java
@@ -1,4 +1,72 @@
 package com.demo.gateway.security;
 
-public class JWTAuthenticationFilter {
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.InvalidClaimException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class JWTAuthenticationFilter extends BasicAuthenticationFilter {
+	private static final Logger log = LoggerFactory.getLogger(JWTAuthenticationFilter.class);
+	private final String TOKEN_HEADER_NAME;
+	private final String TOKEN_HEADER_PREFIX;
+	private final String TOKEN_SECRET;
+
+	public JWTAuthenticationFilter(AuthenticationManager authenticationManager, Environment env) {
+		super(authenticationManager);
+		this.TOKEN_HEADER_NAME = env.getProperty("authorization.token.header.name");
+		this.TOKEN_HEADER_PREFIX = env.getProperty("authorization.token.header.prefix");
+		this.TOKEN_SECRET = env.getProperty("token.secret");
+	}
+
+	private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest req) {
+		final String authorizationHeader = req.getHeader(TOKEN_HEADER_NAME);
+
+		if(authorizationHeader == null) {
+			return null;
+		}
+
+		final String token = authorizationHeader.replace(TOKEN_HEADER_PREFIX, "");
+		String userId = null;
+		try {
+			JWTVerifier verifier = JWT.require(Algorithm.HMAC512(TOKEN_SECRET)).build();
+			DecodedJWT jwt = verifier.verify(token);
+			userId = jwt.getSubject();
+		} catch (JWTVerificationException e) {
+			throw new InvalidClaimException("Invalid token");
+		}
+
+		return userId == null ? null : new UsernamePasswordAuthenticationToken(userId, null, new ArrayList<>());
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		String authorizationHeader = request.getHeader(TOKEN_HEADER_NAME);
+
+		if (authorizationHeader == null || !authorizationHeader.startsWith(TOKEN_HEADER_PREFIX)) {
+			chain.doFilter(request, response);
+			return;
+		}
+
+		UsernamePasswordAuthenticationToken authentication = getAuthentication(request);
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+
+		chain.doFilter(request, response);
+	}
 }
--- gateway/src/main/java/com/demo/gateway/security/WebSecurityConfig.java
+++ gateway/src/main/java/com/demo/gateway/security/WebSecurityConfig.java
@@ -1,4 +1,41 @@
 package com.demo.gateway.security;
 
-public class WebSecurityConfig {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	private final String REGISTRATION_PATH;
+	private final String LOGIN_PATH;
+	private final String ACTUATOR_PATH;
+	private final Environment env;
+
+	@Autowired
+	public WebSecurityConfig(Environment env) {
+		this.env = env;
+		this.REGISTRATION_PATH = env.getProperty("api.registration.url.path");
+		this.LOGIN_PATH = env.getProperty("api.login.url.path");
+		this.ACTUATOR_PATH = env.getProperty("api.zuul.actuator.url.path");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+				.headers().frameOptions().disable()
+				.and()
+				.authorizeRequests()
+				.antMatchers(ACTUATOR_PATH).permitAll()
+				.antMatchers("/user/api/user/login").permitAll()
+				.anyRequest().authenticated()
+				.and()
+				.addFilter(new JWTAuthenticationFilter(authenticationManager(), env))
+				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
 }
